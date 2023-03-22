fd6f783b19a9359033e89d4041466a1a71042d21
Zackary Newman <zpnewman10@gmail.com>
2023-03-22 19:03:03 -0400

    Add support for axis deadzones and format d-pads

--- src/DualShock4Controller.cpp
+++ src/DualShock4Controller.cpp
@@ -7,39 +7,77 @@ void __joystickControllerEmptyCallback(uint16_t __) { }
 void buttonPressed(uint16_t buttons)  __attribute__ ((weak, alias("__joystickControllerEmptyCallback")));
 void buttonReleased(uint16_t buttons)  __attribute__ ((weak, alias("__joystickControllerEmptyCallback")));
 
-void printBuffer(uint32_t len, uint8_t *buf)
-{
-  for (uint8_t i = 0; i < 10; i++) {
-    if (buf[i] < 16) {Serial.print("0");}
-    Serial.print(buf[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.println();
-}
-
-void printBits(size_t const size, void const * const ptr)
-{
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
-
-    for (i = size-1; i >= 0; i--) {
-        for (j = 7; j >= 0; j--) {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
-}
-
-uint16_t isolateButtons(uint8_t * buf)
+uint16_t DualShock4Controller::isolateButtons(uint8_t * buf)
 {
   return ((buf[5] & 0b11110000) << 6) + (buf[6] << 2) + (buf[7] & 0b00000011);
+}
+
+void DualShock4Controller::alignAxes()
+{
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    state.axes[i] ^= 0b10000000;
+  }
+  state.leftStickY = ~state.leftStickY;
+  state.rightStickY = ~state.rightStickY;
+
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    if (-deadzone < state.axes[i] && state.axes[i] < deadzone)
+    {
+      state.axes[i] = 0;
+    }
+  }
+}
+
+void DualShock4Controller::formatDPad()
+{
+  switch (state.dPad)
+  {
+    case 0:
+      dPadAxes[0] = 0;
+      dPadAxes[1] = 1;
+      break;
+    case 1:
+      dPadAxes[0] = 1;
+      dPadAxes[1] = 1;
+      break;
+    case 2:
+      dPadAxes[0] = 1;
+      dPadAxes[1] = 0;
+      break;
+    case 3:
+      dPadAxes[0] = 1;
+      dPadAxes[1] = -1;
+      break;
+    case 4:
+      dPadAxes[0] = 0;
+      dPadAxes[1] = -1;
+      break;
+    case 5:
+      dPadAxes[0] = -1;
+      dPadAxes[1] = -1;
+      break;
+    case 6:
+      dPadAxes[0] = -1;
+      dPadAxes[1] = 0;
+      break;
+    case 7:
+      dPadAxes[0] = -1;
+      dPadAxes[1] = 1;
+      break;
+    default:
+      dPadAxes[0] = 0;
+      dPadAxes[1] = 0;
+      break;
+  }
 }
 
 void DualShock4Controller::Parse(HID *hid, bool is_rpt_id, uint32_t len, uint8_t *buf)
 {
   memcpy(&state, buf, sizeof(state));
+  alignAxes();
+  formatDPad();
 
   uint16_t buttonState = isolateButtons(buf);
 
@@ -57,3 +95,11 @@ void DualShock4Controller::Parse(HID *hid, bool is_rpt_id, uint32_t len, uint8_t
 
   previousButtonState = buttonState;
 };
+
+void DualShock4Controller::setAxisDeadzone(int8_t new_deadzone)
+{
+  if (new_deadzone >= 0)
+  {
+    deadzone = new_deadzone;
+  }
+}
